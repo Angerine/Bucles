4ce416e6433794e5738c790b6c9c0290df68667a
Angerine Martínez Del corral <75480221+Angerine@users.noreply.github.com>
2021-02-08 15:36:55 +0100

    Update Eje3c4.java

--- src/Eje3c4.java
+++ src/Eje3c4.java
@@ -1,7 +1,7 @@
 
-/*3.c.4. Escribir un programa que encuentre el primer número perfecto tras uno inicial indicado por teclado.
-Un número perfecto es un entero positivo que es igual a la suma de todos los enteros positivos (excluido
-él mismo) que son sus divisores. El primer número perfecto es 6, ya que sus divisores son 1, 2, 3 y 1 + 2
+/*3.c.4. Escribir un programa que encuentre el primer nÃºmero perfecto tras uno inicial indicado por teclado.
+Un nÃºmero perfecto es un entero positivo que es igual a la suma de todos los enteros positivos (excluido
+Ã©l mismo) que son sus divisores. El primer nÃºmero perfecto es 6, ya que sus divisores son 1, 2, 3 y 1 + 2
 + 3 = 6.*/
 import java.util.*;
 
@@ -16,7 +16,7 @@ public class Eje3c4 {
 		Scanner respuesta = new Scanner(System.in);
 do {
 	
-		System.out.println("Introduce número");
+		System.out.println("Introduce nÃºmero");
 		num = respuesta.nextInt();
 	
 		if(num<1) {
@@ -44,31 +44,3 @@ num=num+1;
 		} while (perfect == false);
 	}
 }
-/*
- * int numero= pedirNumero(); int perfect=perfecto(numero);
- * System.out.println(perfect);
- * 
- * 
- * 
- * 
- * 
- * } public static int perfecto(int recibido) {
- * 
- * do { recibido=recibido+1; perfect= esPerfecto(recibido);}
- * 
- * while(perfect==false); return recibido;
- * 
- * 
- * }
- * 
- * 
- * public static int pedirNumero() {
- * 
- * do { System.out.println("Introduce número"); num=respuesta.nextInt();
- * if(num<1) { System.out.println("Tiene que mayor a 0"); }} while(num<1);
- * return num;
- * 
- * } public static boolean esPerfecto(int num) { boolean perfecto=false; for(int
- * i=1;i<num;i++) { if(num%i==0) { suma=suma+contador; }} if(suma==num) {
- * perfecto=true; } return perfecto;
- */
