2250fd6da6939e465e3b21d051b3c1d68745269d
Angerine <angerine90@hotmail.com>
2021-02-09 08:14:29 +0100

    Realizado con métodos

--- src/Eje3c4.java
+++ src/Eje3c4.java
@@ -1,8 +1,4 @@
 
-/*3.c.4. Escribir un programa que encuentre el primer número perfecto tras uno inicial indicado por teclado.
-Un número perfecto es un entero positivo que es igual a la suma de todos los enteros positivos (excluido
-él mismo) que son sus divisores. El primer número perfecto es 6, ya que sus divisores son 1, 2, 3 y 1 + 2
-+ 3 = 6.*/
 import java.util.*;
 
 public class Eje3c4 {
@@ -30,7 +26,7 @@ public class Eje3c4 {
 			num = respuesta.nextInt();
 		
 			if(num<1) {
-				System.out.println("Tiene que ser mayor de 0");}
+				System.out.println("Tiene que ser mayor de 0.");}
 			}
 	while(num<1);
 	num=num+1;
@@ -39,6 +35,7 @@ public class Eje3c4 {
 	public static int esPerfecto(int numero) {
 		do {
 			suma=0;
+			
 				for (int i = 1; i < num; i++) {
 					if (num % i == 0) {
 						suma = suma + i;
